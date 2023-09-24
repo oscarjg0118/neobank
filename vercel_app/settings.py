975c49f4d55d4593a73e514e1e1faf81bd57d9c1
Oscar Gonzalez <oscarjg0118@gmail.com>
2023-09-24 14:11:51 -0500

    feat: add documentation

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'users',
     'example'
 ]
@@ -125,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = './static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Neobank API",
+      default_version='v1',
+      description="Documentacion de la API",
+      terms_of_service="",
+      contact=openapi.Contact(email="oscarjg0118gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+#    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', include('example.urls')),
 ]
