133209aef48120f530d6d5fc1452e073ffcd971f
Oscar Gonzalez <oscarjg0118@gmail.com>
2023-09-24 23:03:40 -0500

    feat: get user

--- users/api/router.py
+++ users/api/router.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from users.api.views import RegisterView
+from users.api.views import RegisterView, UserView
 
 urlpatterns = [
     path("auth/register", RegisterView.as_view()),
     path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/me", UserView.as_view()),
 ]
