d5785bd9b7224f8ed8effc88cf788924b520abc0
Oscar Gonzalez <oscarjg0118@gmail.com>
2023-09-24 23:16:01 -0500

    feat: update user

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -21,3 +21,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "email", "username", "first_name", "last_name"]
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name"]
