2f3747955bb7d094ac0a6a3e5bf217b0e8df12de
Oscar Gonzalez <oscarjg0118@gmail.com>
2023-09-25 22:46:59 -0500

    feat: add RetrieveModelMixin to AccountViewSet

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -3,15 +3,20 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import GenericViewSet
 from accounts.api.serializers import AccountSerializer
 from accounts.models import Account
+from accounts.api.permissions import IsAdminUserOrIsOwnerUser
 
 
-class AccountViewSet(mixins.CreateModelMixin, GenericViewSet):
+class AccountViewSet(
+    mixins.CreateModelMixin, mixins.RetrieveModelMixin, GenericViewSet
+):
     serializer_class = AccountSerializer
     queryset = Account.objects.all()
 
     def get_permissions(self):
         if self.action == "create":
             permission_classes = [IsAuthenticated, IsAdminUser]
+        elif self.action == "retrieve":
+            permission_classes = [IsAuthenticated, IsAdminUserOrIsOwnerUser]
         else:
             permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
