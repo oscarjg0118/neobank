45a666f91a6b81451725d996e2642cf1d977bb54
Oscar Gonzalez <oscarjg0118@gmail.com>
2023-09-25 23:51:49 -0500

    refactor: add serializer user to AccountSerializer

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from accounts.models import Account
+from users.api.serializers import UserSerializer
 
 
 class AccountSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Account
         fields = ["id", "user", "balance", "is_active"]
