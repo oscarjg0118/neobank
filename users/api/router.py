65426bd76f4afe628814835d297f4f628f1b16f8
Oscar Gonzalez <oscarjg0118@gmail.com>
2023-09-25 23:43:05 -0500

    refactor: add retrieve to UserView and fix permision

--- users/api/router.py
+++ users/api/router.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me", UserView.as_view()),
+    path("user/<int:user_id>/", UserView.as_view(), name="user-detail-with-id"),
 ]
